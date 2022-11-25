03043ef4b87e9a078399b63a57602019478ba7bf
dahye <>
2022-11-25 20:10:51 +0900

    feat: assignmentService 주입

--- src/main/java/com/dahye/assignment/controller/AssignmentController.java
+++ src/main/java/com/dahye/assignment/controller/AssignmentController.java
@@ -1,17 +1,22 @@
 package com.dahye.assignment.controller;
 
 import com.dahye.assignment.dto.AssignmentRequestDto;
+import com.dahye.assignment.dto.AssignmentResponseDto;
+import com.dahye.assignment.service.AssignmentService;
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@AllArgsConstructor
 @RequestMapping("/api/v1")
 public class AssignmentController {
+    AssignmentService assignmentService;
 
     @PostMapping("/assignment")
-    public String getRequest(@RequestBody AssignmentRequestDto assignmentRequestDto) {
-        return "OK";
+    public AssignmentResponseDto getRequest(@RequestBody AssignmentRequestDto assignmentRequestDto) {
+        return assignmentService.getResponse(assignmentRequestDto);
     }
 }
