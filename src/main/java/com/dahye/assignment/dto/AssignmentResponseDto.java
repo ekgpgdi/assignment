cbd223a956b268b52484c8db9cfaeb79bed27c6e
dahye <>
2022-11-26 00:42:13 +0900

    feat: @Builder 추가

--- src/main/java/com/dahye/assignment/dto/AssignmentResponseDto.java
+++ src/main/java/com/dahye/assignment/dto/AssignmentResponseDto.java
@@ -1,9 +1,13 @@
 package com.dahye.assignment.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @Getter
+@Builder
+@AllArgsConstructor
 @RequiredArgsConstructor
 public class AssignmentResponseDto {
     private String quotient;
